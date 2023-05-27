4d7e106bc5d6dde25b1326718020bfa1b587c931
User <gajanan10@gmail.com>
2023-05-27 20:52:20 +0530

    todo-app

---
+++ src/components/FontAwesome.vue
@@ -0,0 +1,47 @@
+<!-- <template v-slot:item.actions="{ item }">
+   <div>
+    <v-icon
+      small
+      class="mr-2"
+      @click="editItem(item)"
+    >
+      mdi-pencil
+    </v-icon>
+    <v-icon
+      small
+      @click="deleteItem(item)"
+    >
+      mdi-delete
+    </v-icon>
+   </div>
+  </template>
+<script>
+export default{
+    name:'FontAwesome',
+   
+    data(){
+        return{
+            desserts: [],
+            editedItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+        }
+    }
+
+}
+</script> -->
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,81 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+ <v-container>
+  <v-row class="mt-8">
+    <!-- empty column -->
+    <v-col cols="3">      
+    </v-col>
+    <!-- main column -->
+    <v-col cols="6">
+      <div class="d-flex">
+      <v-text-field class="headline" v-model="name"></v-text-field>
+      <v-btn class="mt-3 green white--text" @click="getData">Add me</v-btn>
+      <v-btn class="mt-3 ms-2 red white--text" @click="deleteData">delete me</v-btn>
+      </div>     
+    </v-col>
+    <!-- empty column -->
+    <v-col cols="3">
+    </v-col>
+  </v-row>
+ {{ name }}
+  <!-- table space -->
+  <v-data-table
+  :headers = "headers"
+  :items = "deserts"
+  v-if="addingData=deserts.length>0"
+  item-key="name"
+  show-select
+  single-select
+  class="elevation-1 display-title"
 
+  >
+
+  </v-data-table>
+
+  
+ </v-container>
+ 
+</template>
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+export default{
+  name:'HelloWorld',
+
+  data(){
+    return{
+      name:'',
+      fetcheData:[],
+      seen:false,
+      addingData:false,
+      deserts:[
+        
+      ],
+      headers:[
+        {text:'Task Name', align:'start', value:'name'},
+        // {text:'Category', value:'category'}
+      ]
+    }
+  },
+  methods:{
+    getData(){
+      this.deserts.push({name:this.name})
+      this.name=''
+     
+    },
+    deleteData(){
+      for(let d = 0; d < this.deserts.length; d++){
+    
+    let txt = ''
+
+       txt = this.deserts[d]
+      if(txt === this.name){
+       this.deserts.splice(txt,0)
+      }
+     
+      console.log(txt);
+      
+   }
+    }
+   
   }
+ 
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
